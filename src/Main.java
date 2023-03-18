6044651044b41169d9d800ec1a7073eb9ac07cfc
Mazlum Güneş <gunesmazlum1991@gmail.com>
2023-03-18 16:28:36 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+        int verticalEdge1,verticalEdge2;
+        double hypotenuseEdge;
+
+        System.out.print("1. Enter the perpendicular value: ");
+        verticalEdge1 = input.nextInt();
+
+        System.out.print("2. Enter the perpendicular value:");
+        verticalEdge2 = input.nextInt();
+
+        hypotenuseEdge = Math.sqrt(Math.pow(verticalEdge1,2) + Math.pow(verticalEdge2,2));
+        System.out.println("Hypotenuse value: " + hypotenuseEdge);
     }
 }
